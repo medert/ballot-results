e7ad7798673ed169c85e83fbb3137f421029b899
Meder <m.toktosunov@icloud.com>
2019-01-01 19:15:17 -0500

    4

--- code.rb
+++ code.rb
@@ -46,8 +46,24 @@ puts
   # 3) Which precinct had the highest voter turnout? Use the hash you created in Question 2 to return the answer.
 max = precinct_results.max
 puts "Precinct #{precinct_results.index(max) + 1} had the most votes with #{max} votes"
-
+puts
   # 4) Output the total number of votes per candidate in the format seen above. Create a new hash where the keys name the candidate and the values start at 0. Iterate over the provided `vote_results` to update the totals.
+results = {"Mary Sue" => 0, "Sally Jane" => 0, "Billy Joe" => 0}
+
+vote_results.each do |key, value|
+  value.each do |key, value|
+    case key
+    when "Mary Sue"
+      results["Mary Sue"] += value
+    when "Sally Jane"
+      results["Sally Jane"] += value
+    when "Billy Joe"
+      results["Billy Joe"] += value
+    end
+  end
+end
+
+results.each {|key,value| puts "#{key} had #{value} votes"}
 
   # 5) Who was the winning candidate in Precinct 4 and how many votes did they get?
 
